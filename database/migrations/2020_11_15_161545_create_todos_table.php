4f47b273b9b88400dceb6c0fc36972e7ac0172b6
dhubgrob <dhubgrob@protonmail.com>
2020-11-15 19:22:14 +0100

    2.10

--- database/migrations/2020_11_15_161545_create_todos_table.php
+++ database/migrations/2020_11_15_161545_create_todos_table.php
@@ -15,6 +15,9 @@ class CreateTodosTable extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->boolean('completed');
             $table->timestamps();
         });
     }
---
+++ database/seeders/TodosSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Todo;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class TodosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Todo::factory()->count(10)->create();
+
+
+    }
+}
